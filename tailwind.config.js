e3d46c821856dd988baffa8d8880d65b5e0cad43
IngLeonardo <8471301+IngLeonardo@users.noreply.github.com>
2024-03-09 21:22:59 -0500

    Se realiza la instalacion de tailwind y estructura HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'white':        'hsl(0, 0%, 100%)',
+        'Light-gray':   'hsl(212, 45%, 89%)',
+        'Grayish-blue': 'hsl(220, 15%, 55%)',
+        'Dark blue':    'hsl(218, 44%, 22%)',
+      },
+      fontFamily:{
+        'Outfit' :['Outfit'],
+      },
+      fontSize:{
+        'sm-15': '15px',
+      },
+      screens:{
+        'desktop':{'max':'1440px'},
+        'tablet':{'max':'758px'},
+        'mobile':{'max':'375px'},
+        
+      }
+    },
   },
   plugins: [],
 }
